57fe3cffe6e35cd2fcf7a9ac2c39a9b38044fdf7
akashdhingra <akashdhingra1996@gmail.com>
2021-05-13 12:30:35 +0530

    Implemented the project

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello")
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for info in question_data:
+    obj = Question(info["text"],info["answer"])
+    question_bank.append(obj)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You have completed the Quiz")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
